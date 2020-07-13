cec4acb512b45d802e602e98538bb28d4d800da1
nasirin1 <45781224+nasirin1@users.noreply.github.com>
2020-07-13 12:21:37 +0700

    Update Pulsaku-Untungku.sql

--- Pulsaku-Untungku.sql
+++ Pulsaku-Untungku.sql
@@ -1,1 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Jan 2020 pada 13.16
+-- Versi server: 10.3.16-MariaDB
+-- Versi PHP: 7.1.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `untungku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `nama` varchar(11) NOT NULL,
+  `image` tinyint(1) NOT NULL,
+  `currency` int(255) NOT NULL,
+  `currency_placement` int(255) NOT NULL,
+  `balance` int(255) NOT NULL,
+  `created_at` varchar(255) NOT NULL,
+  `updated_at` varchar(255) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` int(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` int NOT NULL,
+  `remember_token` int NOT NULL,
+  `bio` varchar NOT NULL,
+  `default_currency` int NOT NULL,
+  `show_stats_on_dashboard` int NOT NULL,
+  `created_at` varchar NOT NULL,
+  `updated_at` varchar NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `ID_PENGGUNA` int(11) NOT NULL,
+  `NAMA_PENGGUNA` varchar(255) DEFAULT NULL,
+  `SUREL_PENGGUNA` varchar(255) DEFAULT NULL,
+  `SANDI_PENGGUNA` varchar(255) DEFAULT NULL,
+  `TOKEN_PENGGUNA` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `ID_TRANSAKSI` int(11) NOT NULL,
+  `ID_KATEGORI` int(11) DEFAULT NULL,
+  `ID_PENGGUNA` int(11) DEFAULT NULL,
+  `JENIS_TRANSAKSI` decimal(1,0) DEFAULT NULL,
+  `NOMINAL_TRANSAKSI` varchar(255) DEFAULT NULL,
+  `KETERANGAN_TRANSAKSI` varchar(255) NOT NULL,
+  `WAKTU_TRANSAKSI` datetime(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD PRIMARY KEY (`ID_JADWAL`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`ID_KATEGORI`);
+
+--
+-- Indeks untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`ID_PENGGUNA`),
+  ADD UNIQUE KEY `SUREL_PENGGUNA` (`SUREL_PENGGUNA`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`ID_TRANSAKSI`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `jadwal`
+--
+ALTER TABLE `jadwal`
+  MODIFY `ID_JADWAL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `ID_KATEGORI` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `ID_PENGGUNA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `ID_TRANSAKSI` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=145;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
